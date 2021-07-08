876c75fbf51da54ddac1126e1a40b5494bbfda71
wkwk2805 <wkwk2805@gmail.com>
2021-07-08 13:33:52 +0900

    transaction 이슈 및 유효성검증 해결

--- blockchain-project/wss.js
+++ blockchain-project/wss.js
@@ -1,12 +1,14 @@
 const WebSocket = require("ws");
 const Message = require("./blockchain/message");
+const Transaction = require("./blockchain/transaction");
+const Validation = require("./blockchain/validation");
 const wss = new WebSocket.Server({ port: "8080" });
 let clients = {};
 let blockchain = [];
 
 wss.on("connection", (ws, req) => {
   initialConnection(ws, req);
-  ws.on("message", (data) => onMessage(data, ws));
+  ws.on("message", (data) => onMessage(data, ws, req));
   ws.on("error", onError);
   ws.on("close", () => onClose(req));
   ws.on("open", onOpen);
@@ -18,13 +20,13 @@ const initialConnection = (ws, req) => {
     JSON.stringify(
       new Message({
         action: Message.SAVE_BLOCKCHAIN,
-        data: blockchain.length > 0 && blockchain,
+        data: blockchain.length > 0 ? blockchain : undefined,
       })
     )
   );
 };
 
-const onMessage = (data, ws) => {
+const onMessage = (data, ws, req) => {
   const message = Message.fromJson(data);
   switch (message.action) {
     case Message.SYNC_BLOCKCHAIN:
@@ -33,14 +35,29 @@ const onMessage = (data, ws) => {
         new Message({ action: Message.SAVE_BLOCKCHAIN, data: blockchain })
       );
       break;
-
     case Message.END_MINING:
-      blockchain = message.data;
-      broadcast(new Message({ action: Message.END_MINING, data: blockchain }));
+      if (
+        blockchain.length < message.data.length &&
+        Validation.compareWithAllHashs(message.data)
+      ) {
+        blockchain = message.data;
+        broadcast(
+          new Message({
+            action: Message.ADD_TRANSACTION,
+            data: new Transaction({
+              from: "SYSTEM",
+              to: req.socket.remoteAddress,
+              amount: 50,
+            }),
+          })
+        );
+        broadcast(
+          new Message({ action: Message.END_MINING, data: blockchain })
+        );
+      }
       break;
     case Message.ADD_TRANSACTION:
       broadcast(message);
-
     default:
       break;
   }
