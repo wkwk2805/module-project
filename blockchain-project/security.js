ce2e2ea35f40888e7fec2b54d2d097557f649a71
wkwk2805 <wkwk2805@gmail.com>
2021-07-27 17:01:10 +0900

    key pair pem 으로 변경

--- blockchain-project/security.js
+++ blockchain-project/security.js
@@ -1,25 +1,55 @@
 const crypto = require("crypto");
 
-function getCipherTextByPublicKey(publicKey, plainText) {
-  return crypto
-    .publicEncrypt(publicKey, Buffer.from(plainText, "utf8"))
-    .toString("base64");
+class Security {
+  generateKeyPair() {
+    return crypto.generateKeyPairSync("rsa", {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: "pkcs1",
+        format: "pem",
+      },
+      privateKeyEncoding: {
+        type: "pkcs1",
+        format: "pem",
+      },
+    });
+  }
+
+  getCipherTextByPublicKey(publicKey, plainText) {
+    return crypto
+      .publicEncrypt(
+        { key: publicKey, padding: crypto.constants.RSA_PKCS1_OAEP_PADDING },
+        Buffer.from(plainText)
+      )
+      .toString("base64");
+  }
+
+  getPlainTextByPrivateKey(privateKey, cipherText) {
+    return crypto
+      .privateDecrypt(
+        { key: privateKey, padding: crypto.constants.RSA_PKCS1_OAEP_PADDING },
+        Buffer.from(cipherText, "base64")
+      )
+      .toString("utf8");
+  }
+
+  getCipherTextByPrivateKey(privateKey, plainText) {
+    return crypto
+      .privateEncrypt(
+        { key: privateKey, padding: crypto.constants.RSA_PKCS1_OAEP_PADDING },
+        Buffer.from(plainText, "utf8")
+      )
+      .toString("base64");
+  }
+
+  getPlainTextByPublicKey(publicKey, cipherText) {
+    return crypto
+      .publicDecrypt(
+        { key: publicKey, padding: crypto.constants.RSA_PKCS1_OAEP_PADDING },
+        Buffer.from(cipherText, "base64")
+      )
+      .toString("utf8");
+  }
 }
 
-function getPlainTextByPrivateKey(privateKey, cipherText) {
-  return crypto
-    .privateDecrypt(privateKey, Buffer.from(cipherText, "base64"))
-    .toString("utf8");
-}
-
-function getCipherTextByPrivateKey(privateKey, plainText) {
-  return crypto
-    .privateEncrypt(privateKey, Buffer.from(plainText, "utf8"))
-    .toString("base64");
-}
-
-function getPlainTextByPublicKey(publicKey, cipherText) {
-  return crypto
-    .publicDecrypt(publicKey, Buffer.from(cipherText, "base64"))
-    .toString("utf8");
-}
+module.exports = Security;
